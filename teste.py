86d7016a2297d36f182aad34191d1a86ed2b6272
Rafael Tavares <newsadnessboy@gmail.com>
2022-10-05 19:47:28 -0300

    added: personalização de nome

--- teste.py
+++ teste.py
@@ -2,13 +2,15 @@ numero_texto = 50
 numero = int(numero_texto)
 
 print('-------------------------')
-print('Olá, Vamos ao teste')
+nome = input('Vamos ao teste, antes de comecarmos, insira seu nome: ')
+
+print('Olá', nome, ', tudo bem?')
 print('Tenho um número guardado em minha memória, tente adivinhar.')
 chute = int(input('Para isso, digite um número: '))
 print('-------------------------')
 print("Você digitou o número:",chute)
 if chute == numero:
-    print('Show de bola, você acertou o número secreto!')
+    print('Show de bola', nome,', você acertou o número secreto!')
 else:
-    print('Poxa vida, infelizmente você errou')
+    print('Poxa vida', nome, ', infelizmente você errou')
 print('Obrigado por participar. Tchau!')
